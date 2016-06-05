180ce22224f9c20f852c044e544060fa8da4b1c7
kalper <753494474@qq.com>
2016-06-06 01:02:30 +0800

    代码优化

--- src/main/java/com/springmvc/common/SpringTestCase.java
+++ src/main/java/com/springmvc/common/SpringTestCase.java
@@ -4,8 +4,15 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+/**
+ * 继承此类即可在JUnit Test类中注入相关Service
+ * 
+ * @author kalper
+ * @since 2016年06月06日
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration( locations = {"classpath:conf/spring.xml", "classpath:conf/spring-redis.xml", "classpath:conf/spring-mybatis.xml"})
 public class SpringTestCase {
+	
 	
 }
