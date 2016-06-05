de11b5e53c97e81edb577b8535e6e6cfddeae819
kalper <753494474@qq.com>
2016-06-06 01:25:44 +0800

    代码优化

--- src/main/java/com/springmvc/common/SpringTestCase.java
+++ src/main/java/com/springmvc/common/SpringTestCase.java
@@ -5,7 +5,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
- * 继承此类即可在JUnit Test类中注入相关Service
+ * <b>类说明：</b>继承此类即可在JUnit Test类中注入相关Service
  * 
  * @author kalper
  * @since 2016年06月06日
